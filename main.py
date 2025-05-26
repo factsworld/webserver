3436e5ddae2040dffc86ad6a53bc010bd3cdf169
sanaweb123 <pellaserver@proton.me>
2025-05-26 18:59:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -194,3 +194,8 @@ def delete(filename):
 
 
 # -----------
+# -------------------- RUN APP ON RENDER --------------------
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 5000))  # Render assigns port
+    app.run(debug=False, host='0.0.0.0', port=port)
